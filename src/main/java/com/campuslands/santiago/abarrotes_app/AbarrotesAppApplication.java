dc617a605a43aef50588cf1ed6b3b9d8b48177f2
Santiago <santimh04@gmail.com>
2025-06-06 10:14:01 -0500

    feat: project creation and database linking successful

---
+++ src/main/java/com/campuslands/santiago/abarrotes_app/AbarrotesAppApplication.java
@@ -0,0 +1,11 @@
+package com.campuslands.santiago.abarrotes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbarrotesAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AbarrotesAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/campuslands/santiago/abarrotes_app/AbarrotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslands.santiago.abarrotes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbarrotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
